688a44ad6d70efbb0daa21d58eb13053aa886edf
Ailson Freire <ailson_freire@outlook.com>
2018-02-08 11:09:21 -0300

    swiping only a first card element and rotating statically

--- src/Ball.js
+++ src/Ball.js
@@ -3,13 +3,17 @@ import { View, Animated } from 'react-native';
 
 class Ball extends Component {
     componentWillMount() {
+        //First question of Animation: where is the object
         this.position = new Animated.ValueXY(0, 0);
+        //Second question: where the object should be and the end
         Animated.spring(this.position, {
             toValue: { x: 200, y: 500 }
         }).start();
     }
     render() {
         return (
+            {/*Third question of Animation: 
+            what object should be animaeted*/},
             <Animated.View style={this.position.getLayout()}>
                 <View style={styles.ball} />
             </Animated.View>
--- src/Deck.js
+++ src/Deck.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react';
 import {
     Animated,
-    PanResponder
+    PanResponder,
+    View
 } from 'react-native';
 
 class Deck extends Component {
     constructor(props) {
         super(props);
 
+        //first question of animation: where is te object tha I want do animante?
         const position = new Animated.ValueXY();
-        const panResponder = new PanResponder.create({
+        const panResponder = PanResponder.create({
+            /*Function that is executed any time that user taps on screen
+              setting it true, it meas that it turns all instance of PanResponse
+              to be responsible for the user pressing on the screen*/
             onStartShouldSetPanResponder: () => true,
+            /*PanResponde that chatches all the information when user
+             starts to drag (move) their fingher arround the screen*/
             onPanResponderMove: (event, gesture) => {
+                //Updates that position on Animated.ValeuXY
                 position.setValue({ x: gesture.dx, y: gesture.dy });
             },
+            /*This is call any time that user press, drag and let it go*/
             onPanResponderRelease: () => { }
         });
 
         this.state = { panResponder, position };
     }
+
+    getCardStyle() {
+        return {
+            ...this.state.position.getLayout(),
+            transform: [{ rotate: '45deg' }]
+        };
+    }
     renderCards() {
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
             return this.props.renderCard(item);
         });
     }
 
     render() {
         return (
-            <Animated.View
-                style={this.state.position.getLayout()}
-                {...this.state.panResponder.panHandlers}
-            >
+            <View>
                 {this.renderCards()}
-            </Animated.View>
+            </View>
         );
     }
 }
