1c8f7089112c01debd8077caa5e919e8a318fdf0
Ailson Freire <ailson_freire@outlook.com>
2018-02-10 22:57:25 -0300

    Card swiping out as user drag it to left or right

--- src/Deck.js
+++ src/Deck.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import {
     Animated,
     PanResponder,
-    View
+    View,
+    Dimensions
 } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250; 
 
 class Deck extends Component {
     constructor(props) {
@@ -23,12 +28,25 @@ class Deck extends Component {
                 position.setValue({ x: gesture.dx, y: gesture.dy });
             },
             /*This is call any time that user press, drag and let it go*/
-            onPanResponderRelease: () => { }
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
+            }
         });
 
         this.state = { panResponder, position };
     }
 
+    onSwipeComplete(direction) {
+        const { onSwipeRight, onSwipeLeft } = this.props;
+        direction === 'rigth' ? onSwipeRight() : onSwipeLeft();
+    }
+    
     getCardStyle() {
         /*Return a object with getLayout first properties
           that returns an object that has information about exactly
@@ -36,7 +54,7 @@ class Deck extends Component {
           */
         const { position } = this.state;
         const rotate = position.x.interpolate({
-            inputRange: [-500, 0, 500],
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
             outputRange: ['-120deg', '0deg', '120deg']
         });
         return {
@@ -44,6 +62,21 @@ class Deck extends Component {
             transform: [{ rotate }]
         };
     }
+    /* Combinating onPanRelease to animated.spring to set a initial position on the card */
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: { x: 0, y: 0 }
+        }).start();
+    }
+    /*The same way to above but using timing to set the card out of screen*/
+    forceSwipe(direction) {
+        const x = direction === 'rigth' ? SCREEN_WIDTH : -SCREEN_WIDTH; 
+        Animated.timing(this.state.position, {
+            toValue: { x, y: 0 },
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+    
     renderCards() {
         return this.props.data.map((item, index) => {
             if (index === 0) {
