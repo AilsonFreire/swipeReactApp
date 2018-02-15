556b354359805ef4b20dcbb2808f570c278519a4
Ailson Freire <ailson_freire@outlook.com>
2018-02-14 21:07:31 -0300

    Working with default props, increasing the props.index and updating position

--- src/Deck.js
+++ src/Deck.js
@@ -11,6 +11,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250; 
 
 class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {}
+    }
+
     constructor(props) {
         super(props);
 
@@ -39,12 +44,16 @@ class Deck extends Component {
             }
         });
 
-        this.state = { panResponder, position };
+        this.state = { panResponder, position, index: 0 };
     }
 
     onSwipeComplete(direction) {
-        const { onSwipeRight, onSwipeLeft } = this.props;
-        direction === 'rigth' ? onSwipeRight() : onSwipeLeft();
+        const { onSwipeRight, onSwipeLeft, data } = this.props;
+        const item = data[this.state.index];
+
+        direction === 'rigth' ? onSwipeRight(item) : onSwipeLeft(item);
+        this.state.position.setValue({ x: 0, y: 0 });
+        this.setState({ index: this.state.index + 1 });
     }
     
     getCardStyle() {
@@ -78,8 +87,8 @@ class Deck extends Component {
     }
     
     renderCards() {
-        return this.props.data.map((item, index) => {
-            if (index === 0) {
+        return this.props.data.map((item, i) => {
+            if (i === 0) {
                 return (
                     <Animated.View
                         key={item.id}
